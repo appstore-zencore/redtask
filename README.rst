3e7a6720ee8f5f26b5285f679b0ff14d7d0061b0
zencore <appstore@zencore.cn>
2018-03-07 10:17:18 +0800

    add setup & requirements & travis ci

--- README.rst
+++ README.rst
@@ -1,4 +1,6 @@
 redtask
 =======
 
-Redis task manager
+Redis task manager.
+
+
---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup
+from setuptools import find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.rst'), "r", encoding="utf-8") as fobj:
+    long_description = fobj.read()
+
+requires = [
+]
+
+setup(
+    name="redtask",
+    version="0.1.0",
+    description="Redis task manager.",
+    long_description=long_description,
+    url="https://github.com/appstore-zencore/redtask",
+    author="zencore",
+    author_email="dobetter@zencore.cn",
+    license="MIT",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    keywords=['redtask'],
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    zip_safe=False,
+    requires=requires,
+    install_requires=requires,
+)
